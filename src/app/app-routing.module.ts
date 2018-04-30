8ab0d36d35e0a8607ce7da1193b5f90a4657e9ef
JS8556 <sykora.jakub11@seznam.cz>
2018-04-30 21:01:26 +0200

    green v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
-import { OrdersComponent } from './orders/orders.component';
 
 const routes: Routes = [
   {
